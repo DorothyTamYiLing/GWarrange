eeadc9ac79c44a0b6343c071d4377e588a156f91
DorothyTamYiLing <34043893+DorothyTamYiLing@users.noreply.github.com>
2022-07-20 14:51:38 +0100

    Update extract_flank_coor.py

--- extract_flank_coor.py
+++ extract_flank_coor.py
@@ -1,18 +1,23 @@
+#Usage: python3 extract_flank_coor.py --input allsig_kmer_withN.fasta --outdir output
+
 import argparse
 from Bio import SeqIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", "-i",
-                    help="name of the multifasta kmer file")
+                    help="name of the input sig. kmer multifasta file")
+parser.add_argument("--outdir", "-o",
+                    help="directory where the output file appear")
 args = parser.parse_args()
 
 myinput = args.input
+myoutdir = args.outdir
 
 fasta_sequences = SeqIO.parse(myinput,'fasta') #this is multifasta file of the sig kmers
 #fasta_sequences = SeqIO.parse("kmer_1_2.fasta",'fasta') #this is multifasta file of the sig kmers
 
 
-with open('flank_coor.txt', 'w') as f:
+with open(str(myoutdir)+'/flank_coor.txt', 'w') as f:
 	for record in fasta_sequences:
 		kmerID=record.id
 		#print(kmerID)
@@ -23,6 +28,4 @@ with open('flank_coor.txt', 'w') as f:
 		rightflankstart=mystart+15
 #outputting leftflankend and rightflankstart coordinates
 		f.write(str(kmerID)+"_"+str(leftflankend)+"_"+str(rightflankstart+1)+"_"+str(mylen)+'\n') 
-
-
 
