b9966fa03a94558166ca292cf0d1a1a3bdfb7016
DorothyTamYiLing <34043893+DorothyTamYiLing@users.noreply.github.com>
2022-07-20 14:21:01 +0100

    Update filtering_kmer_and_blast.sh

--- filtering_kmer_and_blast.sh
+++ filtering_kmer_and_blast.sh
@@ -1,14 +1,9 @@
-#usage:bash filtering_kmer_and_blast.sh /home/ubuntu/Dorothy/USAgenomes_GWAS/111_yearGWAS/k200/fix_model/allsig_kmer_withN.fasta \
-#/home/ubuntu/Dorothy/B.pertussis_573genomes_NCBI/Weigand_USA_genome/111_yearGWAS_genlist.fasta.gz 30
+#usage:bash filtering_kmer_and_blast.sh allsig_kmer_withN.fasta 111_yearGWAS_genlist.fasta.gz output 30
 
 ###filtering sig. kmers for blasting by keeping only the kmers that contain flanking sequences (both side) of at least $flnk_len bp in size
 
-#$1 is k_input="/home/ubuntu/Dorothy/USAgenomes_GWAS/111_yearGWAS/k200/fix_model/allsig_kmer_withN.fasta"
-#$2 is gen_input="/home/ubuntu/Dorothy/B.pertussis_573genomes_NCBI/Weigand_USA_genome/111_yearGWAS_genlist.fasta.gz"
-#$3 is flnk_len=30
-
 #get the flank start and end coordinates of the sig kmers, output file name: flank_coor.txt
-python3 extract_flank_coor.py --input $1
+python3 extract_flank_coor.py --input $1 --outdir $3
 
 #making the headerfile based on the number of significant kmers in k_input.fasta
 k_num=$(grep ">" $1 | wc -l)
@@ -18,7 +13,7 @@ let "END=$k_num"  #not including the header line
  
 for (( c=$START; c<=$END; c++ ))
 do
-	echo ">kmer""$c " >> header.txt
+	echo ">kmer""$c " >> $3/header.txt
 done
 
 #remove kmers with short flanks and extract kmers for blasting, by looping through the header file
@@ -28,21 +23,27 @@ mykmer=$(echo ${mykmer} | sed 's/>//')  #remove ">" from the header line
 #mykmer="kmer1" # specify which kmer being process
 #echo ${mykmer}
 
-leftflankend=$(grep ${mykmer}_ flank_coor.txt | cut -d "_" -f2)
-rightflankstart=$(grep ${mykmer}_ flank_coor.txt | cut -d "_" -f3)
-kmerlen=$(grep ${mykmer}_ flank_coor.txt | cut -d "_" -f4)
+leftflankend=$(grep ${mykmer}_ $3/flank_coor.txt | cut -d "_" -f2)
+rightflankstart=$(grep ${mykmer}_ $3/flank_coor.txt | cut -d "_" -f3)
+kmerlen=$(grep ${mykmer}_ $3/flank_coor.txt | cut -d "_" -f4)
 rightlen="$((${kmerlen}-${rightflankstart}+1))"       
 
-if [ ${leftflankend} -lt $3 ] || [ ${rightlen} -lt $3 ]; then   #filter out the kmer with at least one flank of <$flnk_len
-grep ${mykmer}_ flank_coor.txt >> kmer_flanktooshort_flkcoor.txt  #store the flank coordinates of kmer with too short flank
-echo ${mykmer} >> kmer_flanktooshort_4rm.txt   #storing the kmer with flank being too short for remove
+if [ ${leftflankend} -lt $4 ] || [ ${rightlen} -lt $4 ]; then   #filter out the kmer with at least one flank of <$flnk_len
+grep ${mykmer}_ $3/flank_coor.txt >> $3/kmer_flanktooshort_flkcoor.txt  #store the flank coordinates of kmer with too short flank
+echo ${mykmer} >> $3/kmer_flanktooshort_4rm.txt   #storing the kmer with flank being too short for remove
 fi
 
-done  < header.txt #closing for looping through sig kmers header lines]
+done  < $3/header.txt #closing for looping through sig kmers header lines]
 
 #remove the kmer with flanks being too short from the muktifasta file for blasting
-awk 'BEGIN{while((getline<"kmer_flanktooshort_4rm.txt")>0)l[">"$1]=1}/^>/{f=!l[$1]}f' $1 > kmer_forblast.fasta 
+name=$3/kmer_flanktooshort_4rm.txt
+awk -v var="$name" 'BEGIN{while((getline<var)>0)l[">"$1]=1}/^>/{f=!l[$1]}f' $1 > $3/kmer_forblast.fasta 
 
+#remove header.txt
+rm $3/header.txt
 
 ###blasting: querys are kmers in kmer_forblast.fasta; subjects are genomes in gen_input.fasta, 
-blastn -query kmer_forblast.fasta -subject $2 -outfmt 6 -out myout.txt
+mygenome=$2
+gunzip  $mygenome
+blastn -query $3/kmer_forblast.fasta -subject ${mygenome/.gz} -outfmt 6 -out $3/myout.txt
+gzip  ${mygenome/.gz}
