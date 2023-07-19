ea34862e8d5a480be103fc4305067ddbc15f107b
DorothyTamYiLing <34043893+DorothyTamYiLing@users.noreply.github.com>
2023-07-19 22:34:28 +0300

    Update iSreplace_2col.py

--- scripts/iSreplace_2col.py
+++ scripts/iSreplace_2col.py
@@ -21,9 +21,9 @@ parser.add_argument("--outdir", "-o",
 
 args = parser.parse_args()
 
-myinput = args.input  
+myinput = args.input
 myIS= args.coor
-myout= args.outdir 
+myout= args.outdir
 
 mylabel=os.path.basename(str(myIS))
 mylabel_1="_".join(mylabel.split("_")[0:2])
@@ -33,43 +33,47 @@ os.mkdir(str(myout))
 len_rpl=15 #set the length of the replacing string
 newstring="N"*len_rpl
 
-mydf = pd.read_csv(str(myIS),sep='\t',header=0) 
-#mydf = pd.read_csv("myblastout_mergedIS.txt",sep='\t',header=0)  
+mydf = pd.read_csv(str(myIS),sep='\t',header=0)
+#mydf = pd.read_csv("myblastout_mergedIS.txt",sep='\t',header=0)
 
 
 with open(str(myinput), "rt") as handle:
 #with gzip.open(str(myinput), "rt") as handle:
 #with gzip.open("E541_E537_rename.fna.gz", "rt") as handle:
-	fasta_sequences = SeqIO.parse(handle,'fasta')
-	for record in fasta_sequences:
-		print(record.id)
-#		print(repr(record.seq))
-		df=mydf.loc[mydf['sseqid'] == record.id]  #select rows that refer to the sample
-		total_rows = df.shape[0] 
-		mystart=int(df.iloc[0,1]) #for the first IS replacement (first line coor)
-#		print("mystart="+str(mystart))
-		myend=int(df.iloc[0,2]) #for the first IS replacement
-#		print("myend="+str(myend))
-		record.seq=record.seq[:mystart-1] + newstring + record.seq[myend:] #for the first IS replacement
-		lostbp_sum=0
-		mylen=myend-mystart+1 #length of the IS being replaced
-		lostbp=mylen-len_rpl #the numebr of bp lost after this replacement
-		lostbp_sum=lostbp_sum+lostbp #update the total numebr of bp lost after replacement
-		for i in range(1,total_rows):  #from the second IS replacement onwards
-			mystart=int(df.iloc[i,1])
-#			print("mystart="+str(mystart))
-			mynewstart=mystart-lostbp_sum
-#			print("mynewstart="+str(mynewstart))
-			myend=int(df.iloc[i,2])
-#			print("myend="+str(myend))
-			mynewend=myend-lostbp_sum
-#			print("mynewend="+str(mynewend))
-			record.seq=record.seq[:mynewstart-1] + newstring + record.seq[mynewend:]
-			mylen=myend-mystart+1
-			lostbp=mylen-len_rpl #the numebr of bp lost after this replacement
-			lostbp_sum=lostbp_sum+lostbp #update the total numebr of bp lost after replacement
-#			print("lostbp_sum="+str(lostbp_sum))
-		record.description="ISreplaced"
-		SeqIO.write(record, str(myout)+"/"+str(record.id)+"_"+str(mylabel_1)+"_ISreplaced.fasta", "fasta")
-		#SeqIO.write(record, str(record.id)+"_ISreplaced.fasta", "fasta")
-
+        fasta_sequences = SeqIO.parse(handle,'fasta')
+        for record in fasta_sequences:
+                print(record.id)
+#               print(repr(record.seq))
+                df=mydf.loc[mydf['sseqid'] == record.id]  #select rows that refer to the sample
+                total_rows = df.shape[0]
+                if total_rows==0:
+                        print(str(record.id)+" has no IS hit")
+                        record.description="noISfound"
+                        SeqIO.write(record, str(myout)+"/"+str(record.id)+"_"+str(mylabel_1)+"_ISreplaced.fasta", "fasta")
+                else:
+                        mystart=int(df.iloc[0,1]) #for the first IS replacement (first line coor)
+#                       print("mystart="+str(mystart))
+                        myend=int(df.iloc[0,2]) #for the first IS replacement
+#                       print("myend="+str(myend))
+                        record.seq=record.seq[:mystart-1] + newstring + record.seq[myend:] #for the first IS replacement
+                        lostbp_sum=0
+                        mylen=myend-mystart+1 #length of the IS being replaced
+                        lostbp=mylen-len_rpl #the numebr of bp lost after this replacement
+                        lostbp_sum=lostbp_sum+lostbp #update the total numebr of bp lost after replacement
+                        for i in range(1,total_rows):  #from the second IS replacement onwards
+                                mystart=int(df.iloc[i,1])
+#                               print("mystart="+str(mystart))
+                                mynewstart=mystart-lostbp_sum
+#                               print("mynewstart="+str(mynewstart))
+                                myend=int(df.iloc[i,2])
+#                               print("myend="+str(myend))
+                                mynewend=myend-lostbp_sum
+#                               print("mynewend="+str(mynewend))
+                                record.seq=record.seq[:mynewstart-1] + newstring + record.seq[mynewend:]
+                                mylen=myend-mystart+1
+                                lostbp=mylen-len_rpl #the numebr of bp lost after this replacement
+                                lostbp_sum=lostbp_sum+lostbp #update the total numebr of bp lost after replacement
+#                               print("lostbp_sum="+str(lostbp_sum))
+                        record.description="ISreplaced"
+                        SeqIO.write(record, str(myout)+"/"+str(record.id)+"_"+str(mylabel_1)+"_ISreplaced.fasta", "fasta")
+                        #SeqIO.write(record, str(record.id)+"_ISreplaced.fasta", "fasta")
