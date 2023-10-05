dfc4cea32fdb5838e0b9f945369d930cd89a53f2
DorothyTamYiLing <34043893+DorothyTamYiLing@users.noreply.github.com>
2023-10-05 14:46:18 +0100

    Update plot_intactk.R

--- scripts/plot_intactk.R
+++ scripts/plot_intactk.R
@@ -111,7 +111,9 @@ p  <- ggplot(data = myx_unqiue) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
 geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
               data = Rects, fill = "grey50", alpha = 0.1)+
- scale_y_continuous(name="genome phenotype group", breaks=seq(0,1,1))
+ scale_y_continuous(name="genome phenotype group", breaks=seq(0,1,1))+
+labs(caption = "Height of arrows corresponds to proportion of case/control genomes \n K-mer/colour indices refer to the mycol_index column in the corresponding kmer4plot.txt")
 
 p
 dev.off()
+
