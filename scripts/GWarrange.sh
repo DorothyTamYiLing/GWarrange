c246fcc8012c9572f7c4ea865001cb9ce4f8d536
DorothyTamYiLing <34043893+DorothyTamYiLing@users.noreply.github.com>
2024-03-15 12:48:54 +0000

    Update GWarrange.sh

--- scripts/GWarrange.sh
+++ scripts/GWarrange.sh
@@ -2,7 +2,8 @@
 flk_len=30
 dedupk=2
 intkrd=1000
-ext_mrg="100_3"
+ext_mrg_min="100_3"
+ext_mrg_max="7000_200"
 pyseer_arg="--min-af 0.05 --max-af 0.95 --print-samples --no-distances"
 fsmlite_arg="-v -t tmp -m 200 -M 200"
 unitigcaller_arg="--threads 8"
@@ -51,11 +52,16 @@ while test $# -gt 0; do
                     replist=$1
                     shift
                     ;;
-                -ext_mrg)
+                -ext_mrg_min)
                     shift
-                    ext_mrg=$1
+                    ext_mrg_min=$1
                     shift
                     ;;
+		-ext_mrg_max)
+                    shift
+                    ext_mrg_max=$1
+                    shift
+                    ;;   
                 -pyseer_arg)
                     shift
                     pyseer_arg=$1
@@ -101,6 +107,8 @@ echo "fsmlite_arg: $fsmlite_arg";
 echo "unitigcaller_arg: $unitigcaller_arg";
 echo "string_type: $string_type";
 echo "number of thread for BLAST: $thread_blast";
+echo "minimum extension and merge parameters: $ext_mrg_min";
+echo "maximum extension and merge parameters: $ext_mrg_max";
 
 #ext_mrg="100_3"
 #pheno="prn_status_pheno.txt"
@@ -109,13 +117,6 @@ echo "number of thread for BLAST: $thread_blast";
 #replist="IS_NZ_CP025371.1.fasta"
 #string_type="kmer"
 
-#split the extend and merge parameter into different variables, default=100_3
-extend_para=$(echo $ext_mrg | cut -d '_' -f 1)
-merge_para=$(echo $ext_mrg | cut -d '_' -f 2)
-
-#echo extend and merge parameters, default=100_3
-echo "extend parameter : $extend_para";
-echo "merge parameter : $merge_para";
 
 #adding header to phenotype file for pyseer input format
 echo "samples binary" | cat - example_data/$pheno > example_data/${pheno/.txt}_4pyseer.txt
@@ -149,20 +150,25 @@ echo "${pyseer_arg}"
 echo "${fsmlite_arg}"
 echo "${unitigcaller_arg}"
 
-echo "run extmerge2pyseer.sh using default parameters"
+echo "run extmerge2pyseer.sh using minimum extend merge parameters"
+
+min_ext=$(echo ${ext_mrg_min} | cut -d "_" -f1)
+min_mrg=$(echo ${ext_mrg_min} | cut -d "_" -f2)
+max_ext=$(echo ${ext_mrg_max} | cut -d "_" -f1)
+max_mrg=$(echo ${ext_mrg_max} | cut -d "_" -f2)
 
 bash scripts/extmerge2pyseer.sh \
--extend_para 100 -merge_para 3 \
+-extend_para ${min_ext} -merge_para ${min_mrg} \
 -pheno ../example_data/${pheno/.txt}_4pyseer.txt \
 -pyseer_arg "${pyseer_arg}" \
 -fsmlite_arg "${fsmlite_arg}" \
 -unitigcaller_arg "${unitigcaller_arg}" \
 -string_type ${string_type}
 
-echo "run extmerge2pyseer.sh using alternative parameters"
+echo "run extmerge2pyseer.sh using maximum extend merge parameters"
 
 bash scripts/extmerge2pyseer.sh \
--extend_para ${extend_para} -merge_para ${merge_para} \
+-extend_para ${max_ext} -merge_para ${max_mrg} \
 -pheno ../example_data/${pheno/.txt}_4pyseer.txt \
 -pyseer_arg "${pyseer_arg}" \
 -fsmlite_arg "${fsmlite_arg}" \
@@ -175,21 +181,23 @@ echo "build database for efficient blasting"
 makeblastdb -in example_data/${gen/.gz} -dbtype nucl -out genome_db
 
 #calculate value for d flag
-default_cut=$(grep "Maximum size" ext100_merge3_mergedISstat.txt | cut -d$'\t' -f 2)
+default_cut=$(grep "Maximum size" ext${min_ext}_merge${min_mrg}_mergedISstat.txt | cut -d$'\t' -f 2)
 default_d=$(awk "BEGIN { print int(${default_cut}*1.2)}")
 
-alt_cut=$(grep "Maximum size" ext${extend_para}_merge${merge_para}_mergedISstat.txt | cut -d$'\t' -f 2)
+alt_cut=$(grep "Maximum size" ext${max_ext}_merge${max_mrg}_mergedISstat.txt | cut -d$'\t' -f 2)
 alt_d=$(awk "BEGIN { print int(${alt_cut}*1.2)}")
 
 
 #running main.sh For ext100_merge3_ISreplaced_genomes set unitigs
-bash scripts/main.sh -k ext100_merge3_ISreplaced_genomes/final_sig.fasta \
+bash scripts/main.sh -k ext${min_ext}_merge${min_mrg}_ISreplaced_genomes/final_sig.fasta \
 -g genome_db \
 -p example_data/${pheno} -d ${default_d} -f ${flk_len} \
--o ${gen/.fna.gz}_ext100_merge3_outdir -s ${gen_size} -x ${dedupk} -y ${intkrd} -t ${thread_blast}
+-o ${gen/.fna.gz}_ext${min_ext}_merge${min_mrg}_outdir -s ${gen_size} -x ${dedupk} -y ${intkrd} -t ${thread_blast}
 
 #running main.sh For ext7000_merge200_ISreplaced_genomes set unitigs
-bash scripts/main.sh -k ext${extend_para}_merge${merge_para}_ISreplaced_genomes/final_sig.fasta \
+bash scripts/main.sh -k ext${max_ext}_merge${max_mrg}_ISreplaced_genomes/final_sig.fasta \
 -g genome_db \
 -p example_data/${pheno} -d ${alt_d} -f ${flk_len} \
--o ${gen/.fna.gz}_ext${extend_para}_merge${merge_para}_outdir -s ${gen_size} -x ${dedupk} -y ${intkrd} -t ${thread_blast}
+-o ${gen/.fna.gz}_ext${max_ext}_merge${max_mrg}_outdir -s ${gen_size} -x ${dedupk} -y ${intkrd} -t ${thread_blast}
+
+
