b276984a5155f570199c28fa5fd448f5ad9cd9ce
DorothyTamYiLing <34043893+DorothyTamYiLing@users.noreply.github.com>
2024-03-24 09:35:39 +0000

    Update extmerge2pyseer.sh

--- scripts/extmerge2pyseer.sh
+++ scripts/extmerge2pyseer.sh
@@ -51,6 +51,11 @@ while test $# -gt 0; do
                     string_type=$1
                     shift
 		    ;;
+            -thread)
+                    shift
+                    thread=$1
+                    shift
+            ;;
                 *)
                    echo "$1 is not a recognized flag!"
                    return 1;
@@ -58,25 +63,35 @@ while test $# -gt 0; do
           esac
   done  
 
-echo "checking extmerge2pyseer_script parameters"
+#echo "checking extmerge2pyseer_script parameters"
 echo "extend_para: $extend_para";
 echo "merge_para: $merge_para";
-echo "klen: $klen";
-echo "minor: $minor";
-echo "major: $major";
-echo "pheno: $pheno";
-echo "pyseer_arg: $pyseer_arg";
-echo "fsmlite_arg: $fsmlite_arg";
-echo "unitigcaller_arg: $unitigcaller_arg";
-echo "string_type: $string_type";
+#echo "pyseer_arg: $pyseer_arg";
+#echo "fsmlite_arg: $fsmlite_arg";
+#echo "unitigcaller_arg: $unitigcaller_arg";
+#echo "string_type: $string_type";
+#echo "thread: $thread";
 
 #make genome set using extension and merging parameters
 echo "making genome set with extension and merge parameters"
 Rscript scripts/merge_IS.R --input blastrep_out.txt --extend ${extend_para} --merge ${merge_para}
-python3 scripts/iSreplace_2col.py --input fixed_genomes.fasta --coor ext${extend_para}_merge${merge_para}_mergedIS.txt --out ext${extend_para}_merge${merge_para}_ISreplaced_genomes
+
+#create output directory, replace old one if exists
+if [[ -d ext${extend_para}_merge${merge_para}_ISreplaced_genomes_${string_type} ]]; then
+        echo "directory exists, replacing with the new one"
+        rm -r ext${extend_para}_merge${merge_para}_ISreplaced_genomes_${string_type}
+        python3 scripts/iSreplace_2col.py --input fixed_genomes.fasta --coor ext${extend_para}_merge${merge_para}_mergedIS.txt --out ext${extend_para}_merge${merge_para}_ISreplaced_genomes
+else
+        python3 scripts/iSreplace_2col.py --input fixed_genomes.fasta --coor ext${extend_para}_merge${merge_para}_mergedIS.txt --out ext${extend_para}_merge${merge_para}_ISreplaced_genomes
+
+fi
+
+
+#rename to add string type
+mv ext${extend_para}_merge${merge_para}_ISreplaced_genomes ext${extend_para}_merge${merge_para}_ISreplaced_genomes_${string_type}
 
 #enter into the directory of genome set
-cd ext${extend_para}_merge${merge_para}_ISreplaced_genomes
+cd ext${extend_para}_merge${merge_para}_ISreplaced_genomes_${string_type}
 
 #process kmers
 #generating fsm-ite input file
@@ -89,7 +104,7 @@ echo "run pyseer on fsm-lite output"
 eval "pyseer --phenotypes ${pheno} \
 --kmers fsm_output.txt.gz \
 --output-patterns kmer_patterns.txt \
-${pyseer_arg} > kmer_pyseer"
+${pyseer_arg} --cpu ${thread} > kmer_pyseer"
 
 #get p threshold
 python3 ../scripts/count_patterns.py kmer_patterns.txt > count_pattern.txt
@@ -100,6 +115,16 @@ mythres=$(grep "Threshold" count_pattern.txt | cut -f2 -d$':' )
 #get sigk
 awk -v var="${mythres}" '{ if ($4 <= var) { print } }' kmer_pyseer > sigk_pyseer.txt #kmer
 
+
+#check if the file is empty
+if [ -s sigk_pyseer.txt ]; then
+
+sigkcount=$(wc -l sigk_pyseer.txt)
+
+myk="yes"
+
+echo "there are "${sigkcount}" significant kmers found"
+
 #convert to fasta format
 awk '{print $1}' sigk_pyseer.txt > sigk_seq.txt 
 
@@ -108,39 +133,65 @@ number=$(cat sigk_seq.txt | wc -l)
 
 #remove any existing header file
 if ! [ -f header.txt ]; then
-  echo "header file does not exist."
+  echo "there is no header file"
 else
+    echo "remove existing header file"
   rm header.txt
 fi
 
-START=1
-let "END=$number" 
- 
-for (( c=$START; c<=$END; c++ ))
-do
-    echo ">kmer""$c " >> header.txt
-done
+seq 1 ${number} | awk '$1=">kmer"$1' > header.txt
 
-paste -d \\n header.txt sigk_seq.txt > final_sig.fasta
+paste -d \\n header.txt sigk_seq.txt > sigk_seq.fasta
 
-#classifying kmers into those containing "N" and those do not contain "N". Produce "sigk_noN.fasta" and "sigk_withN.fasta".
-python3 ../scripts/class_k.py --input final_sig.fasta --outdir .
+#create the final fasta, can overwrite it later
+cp sigk_seq.fasta final_sig.fasta
+
+#for combine with sig uni classifying kmers into those containing "N" and those do not contain "N". Produce "sigk_noN.fasta" and "sigk_withN.fasta"
+#there is always output, just may produce empty file
+python3 ../scripts/class_k.py --input sigk_seq.fasta --outdir .
+
+sigkN=$(wc -l sigk_withN.fasta)
+sigknoN=$(wc -l sigk_noN.fasta)
+
+echo "there are "${sigkN}" significant kmers with N"
+echo "there are "${sigknoN}" significant kmers without N"
 
 #rename
 mv sigk_noN.fasta classk_noN.fasta 
 mv sigk_withN.fasta classk_withN.fasta
 
+#add kmer names to pyseer output
+sed 's/>//g' header.txt > header_1.txt
+paste header_1.txt sigk_pyseer.txt > sigk_pyseer_1.txt
+rm header_1.txt
 
+#add header line
+pyhead=$(head -1 kmer_pyseer)
+pyhead_1="kmer_ID   "${pyhead}   
+echo ${pyhead_1} >  pyhead.txt
+cat pyhead.txt sigk_pyseer_1.txt > sigk_pyseer.txt
+rm pyhead.txt
+rm sigk_pyseer_1.txt
+
+
+else
+    echo "no significant kmer found"
+    myk="no"
+fi  #close bracket for if [ -s sigk_pyseer.txt ]; then
+
+
+
+#################################
 
 #process unitigs
-if [ ${string_type} = "kmer_and_unitigs" ]
+if [ ${string_type} = "kmers_and_unitigs" ]
 then
 
 echo "generating unitig-caller input file"
 #generating unitig-caller input file
 ls -d -1 $PWD/*_ISreplaced.fasta > input.txt
 #running unitig-caller
-eval "unitig-caller --call --pyseer --refs input.txt --out unitigcall_out ${unitigcaller_arg}"
+eval "unitig-caller --call --pyseer --refs input.txt --out unitigcall_out --threads ${thread} ${unitigcaller_arg}"
 #fixing sample names in output file for pyseer
 sed "s/_ext${extend_para}_merge${merge_para}_ISreplaced//g" unitigcall_out.pyseer > unitigcall.pyseer
 gzip unitigcall.pyseer
@@ -150,7 +201,7 @@ echo "run pyseer on unitig-caller output"
 eval "pyseer --phenotypes ${pheno} \
 --kmers unitigcall.pyseer.gz \
 --output-patterns unitig_patterns.txt \
-${pyseer_arg} > unitig_pyseer"
+${pyseer_arg} --cpu ${thread} > unitig_pyseer"
 
 #get p threshold
 python3 ../scripts/count_patterns.py unitig_patterns.txt > count_uni_pattern.txt
@@ -161,6 +212,15 @@ mythres_uni=$(grep "Threshold" count_uni_pattern.txt | cut -f2 -d$':' )
 #get sig unitig
 awk -v var="${mythres_uni}" '{ if ($4 <= var) { print } }' unitig_pyseer > siguni_pyseer.txt #unitig
 
+#check if the file is filled
+if [ -s siguni_pyseer.txt ]; then
+
+myuni="yes"
+
+sigunicount=$(wc -l siguni_pyseer.txt)
+
+echo "there are "${sigunicount}" significant unitigs found"
+
 #convert to fasta format, unitigs
 awk '{print $1}' siguni_pyseer.txt > siguni_seq.txt 
 
@@ -169,39 +229,62 @@ number=$(cat siguni_seq.txt | wc -l)
 
 #remove any existing header file
 if ! [ -f header.txt ]; then
-  echo "header file does not exist."
+  echo "there is no header file"
 else
+    echo "remove existing header file"
   rm header.txt
 fi
 
-START=1
-let "END=$number" 
- 
-for (( c=$START; c<=$END; c++ ))
-do
-    echo ">unitig""$c " >> header.txt
-done
+seq 1 ${number} | awk '$1=">unitig"$1' > header.txt
 
 paste -d \\n header.txt siguni_seq.txt > siguni_seq.fasta
 
-#add kmer names to pyseer output
+#classifying unitigs into those containing "N" and those do not contain "N". Produce "sigk_noN.fasta" and "sigk_withN.fasta".
+#python3 ../scripts/class_k.py --input siguni_seq.fasta --outdir .
+
+#add unitig names to pyseer output
 sed 's/>//g' header.txt > header_1.txt
-paste header_1.txt sigk_pyseer.txt > sigk_pyseer_1.txt & mv sigk_pyseer_1.txt sigk_pyseer.txt
+paste header_1.txt siguni_pyseer.txt > siguni_pyseer_1.txt
 rm header_1.txt
 
 #add header line
-pyhead=$(head -1 kmer_pyseer)
+pyhead=$(head -1 unitig_pyseer)
 pyhead_1="kmer_ID   "${pyhead}   
 echo ${pyhead_1} >  pyhead.txt
-cat pyhead.txt sigk_pyseer.txt > sigk_pyseer_1.txt mv sigk_pyseer_1.txt sigk_pyseer.txt
+cat pyhead.txt siguni_pyseer_1.txt > siguni_pyseer.txt
+rm pyhead.txt
+rm siguni_pyseer_1.txt
 
-#classifying unitigs into those containing "N" and those do not contain "N". Produce "sigk_noN.fasta" and "sigk_withN.fasta".
-python3 ../scripts/class_k.py --input siguni_seq.fasta --outdir .
+else
 
-# concatenatingn sig. kmers with N and sig. unitigs, final_sig.fasta will overwrite the previous one from kmers
+    echo "no significant unitig found"
+    myuni="no"
+
+fi #close bracket for if [ -s siguni_pyseer.txt ]; then
+
+
+#echo $myk $myuni
+
+#if there is sig kmer but no sig uni, do nothing, keeping using final_seq.fasta generated from before
+
+#if there is sig kmer and sig uni, combine sigk with N and all sig uni, overwrite final_seq.fasta
+if [ ${myk} = "yes" ] && [ ${myuni} = "yes" ]; then
+    echo "there are both significant kmers and significant unitigs"
+    echo "combining significant kmers with N and significant unitigs for rearrangement detection"
 cat classk_withN.fasta siguni_seq.fasta > final_sig.fasta
+fi
 
+if [ ${myk} == "no" ] && [ ${myuni} == "no" ]; then
+echo "no significant kmer nor significant unitig found"
+touch final_sig.fasta  #create an empty file anyway
 fi
 
 
+fi #close bracket for if [ ${string_type} = "kmer_and_unitigs" ]
+
+#reset everything
+myk="none"
+myuni="none"
+
 cd ..
+
